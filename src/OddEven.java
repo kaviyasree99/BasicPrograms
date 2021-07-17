f3481f06122ede1a79ce79226df02dfefdfb8b7b
kaviyasree99 <kaviyarajalingam@gmail.com>
2021-07-17 15:32:58 +0530

    Added:Basic Program added

---
+++ src/OddEven.java
@@ -0,0 +1,21 @@
+
+public class OddEven {
+	
+    public static void main(String[] args)
+    {
+        
+        int num = 555;
+  
+        if (num % 2 == 0) {
+  
+        
+            System.out.println(" Number is Even");
+        }
+        else {
+  
+            
+            System.out.println("Number is odd");
+        }   
+    }
+  
+}
---
+++ src/swaptwonumbers.java
@@ -0,0 +1,22 @@
+
+public class swaptwonumbers {
+	public static void main(String[] args)
+    {
+ 
+        int a = 333, b = 111;
+ 
+        System.out.println("Before Swap");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+ 
+        int temp = a;
+        a = b;
+        b = temp;
+ 
+        System.out.println("After swap");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
+
+
